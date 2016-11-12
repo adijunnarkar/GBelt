0583f7209ef3a4601102a05b25a66d114d71acad
adijunnarkar <adityajunnarkar@hotmail.com>
2016-11-12 17:59:53 -0500

    Added bluetooth arduino code to my branch

---
+++ bluetooth_module/bluetooth_module.ino
@@ -0,0 +1,41 @@
+int led = 13;
+//Char used for reading in Serial characters
+char inbyte = 0;
+String one_transmission = "";
+bool start_reading_data = false;
+//*******************************************************************************************
+
+void setup() {
+  // initialise serial communications at 9600 bps:
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+}
+
+void loop() {
+  if (Serial.available() > 0)
+  {
+    inbyte = Serial.read();
+    if (inbyte == '#')
+    {
+      start_reading_data = true;
+      //LED off
+      //digitalWrite(led, LOW);
+    }
+    if (start_reading_data)
+    {
+      one_transmission += inbyte;
+    }
+    if (inbyte == '~')
+    {
+      Serial.println(one_transmission);
+      one_transmission = "";
+      start_reading_data = false;
+      //LED on
+      //digitalWrite(led, HIGH);
+    }
+  }
+  //delay by 2s. Meaning we will be sent values every 2s approx
+  //also means that it can take up to 2 seconds to change LED state
+  //delay(100);
+}
